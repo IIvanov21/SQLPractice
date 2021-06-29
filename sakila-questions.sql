b2fcc0662a4b6b5103bb5c86b96dda551329847d
iivanov21 <iivanov21@hotmail.com>
2021-06-29 14:08:40 +0100

    II: Completed sakila up to question 10

--- sakila-questions.sql
+++ sakila-questions.sql
@@ -1,7 +1,14 @@
-/* Sakila challenge question 1-5 */
+/* Sakila challenge question 1-6 */
 USE sakila;
 SELECT * FROM actor;
+SELECT * FROM actor WHERE first_name = 'John';
 SELECT * FROM actor WHERE last_name='Neeson';
 SELECT * FROM actor WHERE (actor_id % 10) = 0;
 SELECT description FROM film WHERE film_id=100; 
 SELECT * FROM film WHERE rating = 'R';
+/* Sakila questions 7-10 */
+SELECT * FROM film WHERE rating != 'R';
+SELECT * FROM film ORDER BY length ASC LIMIT 10;
+SELECT * FROM film WHERE length=(SELECT MAX(length)FROM film);
+SELECT * FROM film WHERE special_features='Deleted Scenes';
+/* Sakila questions 11-15 */
