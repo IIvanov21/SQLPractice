c22ea0f165a92e2d4804a1db1db63403bfb1e80d
iivanov21 <iivanov21@hotmail.com>
2021-06-29 15:09:26 +0100

    II: Answered questions up to 15 on sakila!

--- sakila-questions.sql
+++ sakila-questions.sql
@@ -12,3 +12,14 @@ SELECT * FROM film ORDER BY length ASC LIMIT 10;
 SELECT * FROM film WHERE length=(SELECT MAX(length)FROM film);
 SELECT * FROM film WHERE special_features='Deleted Scenes';
 /* Sakila questions 11-15 */
+SELECT last_name FROM actor GROUP BY last_name HAVING  COUNT(last_name)=1 ORDER BY last_name DESC; 
+SELECT last_name FROM actor GROUP BY last_name HAVING COUNT(last_name)>1 ORDER BY last_name ASC;
+/*Question Answer: 13.Which actor has appeared in the most films?*/
+SELECT a.actor_id, a.first_name, a.last_name
+FROM actor a
+JOIN film_actor f ON a.actor_id=f.actor_id
+group by a.actor_id, a.last_name
+order by count(*) desc ;
+
+SELECT release_year FROM film WHERE title='Academy Dinosaur';
+SELECT AVG(length) FROM film;
